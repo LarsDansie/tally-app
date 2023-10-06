6b2b50b6a59ce9d8bb713f5c94c183cfd11706c0
Lars Dansie <larswdansie@gmail.com>
2023-10-06 07:56:21 -0600

    checking if this is set up - first commit I guess

--- Tally App/ViewController.swift
+++ Tally App/ViewController.swift
@@ -8,11 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var tallyLabel: UILabel!
+    @IBOutlet weak var tallyAmountInput: UITextField!
+    
+    var tallyCount: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func tallyButtonPressed(_ sender: UIButton) {
+        if let tallyAmount = tallyAmountInput.text, let incrementValue = Int(tallyAmount) {
+                tallyCount += incrementValue
+                tallyLabel.text = "\(tallyCount)"
+            }
+    }
+    
+    func tallyUp() {
+    }
+    
 
 
 }
